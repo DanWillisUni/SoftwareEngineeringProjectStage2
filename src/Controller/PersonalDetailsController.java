0eacf72b8193fdc63ffad7c00326c9888198f2e9
DanWillisUni <dan.willis.uni@gmail.com>
2020-04-29 18:26:45 +0100

    added height into personal details

--- src/Controller/PersonalDetailsController.java
+++ src/Controller/PersonalDetailsController.java
@@ -39,6 +39,7 @@ public class PersonalDetailsController {
     @FXML private PasswordField password2;
     @FXML private DatePicker DOB;
     @FXML private ComboBox gender;
+    @FXML private TextField height;
     @FXML private Label errorMsg;
     @FXML private Label currentForename;
     @FXML private Label currentSurname;
@@ -46,6 +47,7 @@ public class PersonalDetailsController {
     @FXML private Label currentEmail;
     @FXML private Label currentDOB;
     @FXML private Label currentGender;
+    @FXML private Label currentHeight;
     /**
      * sets the user to the user that is logged in
      * @param User Person object logged in
@@ -69,6 +71,9 @@ public class PersonalDetailsController {
         currentEmail.setText(User.getEmail());
         currentDOB.setText(User.getDOB().toString());
         currentGender.setText(Character.toString(User.getGender()));
+        if (User.getHeight()>0){
+            currentHeight.setText(Integer.toString(User.getHeight()));
+        }
     }
     @FXML
     private void SaveUser (ActionEvent event) {
@@ -185,6 +190,21 @@ public class PersonalDetailsController {
                 }
             }
         }
+        //height validation
+        if (height.getText().matches("^([1-9][0-9]*(\\.[0-9]+)?|0+\\.[0-9]*[1-9][0-9]*)$")){
+            int i = Integer.parseInt(height.getText());
+            if (i>250){
+                errorMsg.setText("Error: height greater than 250");
+                height.setText("");
+            } else {
+                User.setHeight(i);
+            }
+        } else {
+            errorMsg.setText("Error: height not positive");
+            height.setText("");
+        }
+
+
 
         if (errorMsg.getText().equals("")){
             User.update();
