8b0aa7b2d7bf89bc7f654a5c32c0f61602f73e28
DanWillisUni <dan.willis.uni@gmail.com>
2020-05-06 22:42:07 +0100

    commented weight goal

--- src/Model/WeightGoal.java
+++ src/Model/WeightGoal.java
@@ -12,13 +12,22 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class WeightGoal {
-    int id;
-    User user;
-    double targetWeight;
-    Date set;
-    Date due;
+    int id;//id of the goal
+    User user;//user the goal applies to
+    double targetWeight;//target weight for the user
+    Date set;//date the goal was set
+    Date due;//date the goal is due
     boolean toLoose;//true if the goal is to get under targetweight
 
+    /**
+     * Constructor
+     * @param id weight goal
+     * @param user user obj that the goal pertains to
+     * @param targetWeight the target weight of the goal
+     * @param set the date the goal was set
+     * @param due the date the goal is due
+     * @param toLoose if the goal is to loose
+     */
     WeightGoal(int id, User user, double targetWeight, Date set, Date due, boolean toLoose){
         this.id = id;
         this.user=user;
@@ -27,6 +36,13 @@ public class WeightGoal {
         this.due=due;
         this.toLoose=toLoose;
     }
+    /**
+     * Constructor used to make a new goal
+     * @param user user obj that the goal pertains to
+     * @param targetWeight the target weight of the goal
+     * @param due the date the goal is due
+     * @param toLoose if the goal is to loose
+     */
     public WeightGoal(User user, double targetWeight, Date due, boolean toLoose){
         GenericDatabaseController db = new GenericDatabaseController();
         this.id = db.genID("GoalWeight","idGoalWeight");
@@ -37,29 +53,62 @@ public class WeightGoal {
         this.toLoose=toLoose;
     }
 
+    /**
+     * Get the id of the goal
+     * @return the id of the goal
+     */
     public int getId() {
         return id;
     }
+    /**
+     * Get the user obj
+     * @return the user obj of the goal
+     */
     public User getUser() {
         return user;
     }
+    /**
+     * Get the target weight
+     * @return the target weight
+     */
     public double getTargetWeight() {
         return targetWeight;
     }
+    /**
+     * Get the date the goal was set on
+     * @return the date the goal was set on
+     */
     public Date getSet() {
         return set;
     }
+    /**
+     * Get the date the goal is due on
+     * @return the date the goal is due to be completed by
+     */
     public Date getDue() {
         return due;
     }
+    /**
+     * Get the due date
+     * Convert it to a string in dd-mm-yyyy format
+     * @return the due date as a string
+     */
     public String getDueStr(){
         DateFormat newFormat = new SimpleDateFormat("dd-MM-yyyy");
         return  newFormat.format(getDue());
     }
+    /**
+     * Get toloose
+     * @return to loose
+     */
     public boolean getToLoose() {
         return toLoose;
     }
 
+    /**
+     * Determines if the goal is met by the user
+     * @return true if the goal is completed
+     */
     public boolean isMet(){
         if (user.getWeight()>0){
             if (user.getWeight() >= targetWeight){
@@ -70,16 +119,27 @@ public class WeightGoal {
         }
         return false;
     }
+    /**
+     * Get if the due date of the goal is due in the past
+     * @return true if the goal was due in the past
+     */
     public boolean isOverdue(){
         Date today = new Date();
         return (today.getTime()>due.getTime());
     }
+    /**
+     * Works out the number of days till when the goal is due
+     * @return the number of days till the goal
+     */
     public int daysTillGoal(){
         Date current = new Date();
         long differance = getDue().getTime() - current.getTime();
         return Integer.parseInt(Long.toString(differance/86400000));
     }
 
+    /**
+     * Adds the goal to the database
+     */
     public void add(){
         GenericDatabaseController db = new GenericDatabaseController();
         try {
@@ -93,11 +153,19 @@ public class WeightGoal {
             e.printStackTrace();
         }
     }
+    /**
+     * Removes the goal from the database
+     */
     public void remove(){
         GenericDatabaseController db = new GenericDatabaseController();
         db.remove(getId(),"goalweight","idgoalweight");
     }
 
+    /**
+     * Gets all the goals of a user
+     * @param u user
+     * @return arraylist of all the users goal
+     */
     public static ArrayList<WeightGoal> getAll(User u){
         GenericDatabaseController db = new GenericDatabaseController();
         ArrayList<WeightGoal> r =  new ArrayList<>();
