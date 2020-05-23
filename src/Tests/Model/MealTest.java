03c77b942bdc768f82f01078f4c0b8c470ff58f4
DanWillisUni <dan.willis.uni@gmail.com>
2020-05-23 13:19:01 +0100

    meal tests

--- src/Tests/Model/MealTest.java
+++ src/Tests/Model/MealTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 
 import java.sql.Connection;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
 public class MealTest {
     static Connection c;
     static Meal a;
@@ -15,34 +18,49 @@ public class MealTest {
     public void setUp() {
         GenericDatabaseController genericController = new GenericDatabaseController();
         c=genericController.getConnection();
-        fooda = new FoodItem(11,"apple",95,"");
+        fooda = new FoodItem(0,"apple",95,"");
         a = new Meal(35,fooda,1,"Dinner");//is in database
-        foodb = new FoodItem(60,"Sandwich",260,"Ham and Cheese");
+        foodb = new FoodItem(70,"Sandwich",260,"Ham and Cheese");
         b = new Meal(300,foodb,1,"Lunch");//is not in database
     }
 
     @Test
     public void getId() {
+        int act = a.getId();
+        assertThat(act,is(35));
     }
 
     @Test
     public void getFood() {
+        FoodItem act = b.getFood();
+        assertThat(act.getId(),is(foodb.getId()));
+        assertThat(act.getName(),is(foodb.getName()));
+        assertThat(act.getAmountOfCalories(),is(foodb.getAmountOfCalories()));
+        assertThat(act.getDescription(),is(""));
     }
 
     @Test
     public void getQuantity() {
+        int act = b.getQuantity();
+        assertThat(act,is(1));
     }
 
     @Test
     public void getType() {
+        String act = a.getType();
+        assertThat(act,is("Dinner"));
     }
 
     @Test
     public void getFoodName() {
+        String act = b.getFoodName();
+        assertThat(act,is("Sandwich"));
     }
 
     @Test
     public void getCalories() {
+        int act = a.getCalories();
+        assertThat(act,is(95));
     }
 
 //    @Test
@@ -51,25 +69,45 @@ public class MealTest {
 
     @Test
     public void getMeal() {
+        Meal act = Meal.getMeal(fooda,1,"Dinner",c);
+        assertThat(act.getId(),is(35));
+        assertThat(act.getFoodName(),is("apple"));
+        assertThat(act.getQuantity(),is(1));
+        assertThat(act.getCalories(),is(95));
+        assertThat(act.getType(),is("Dinner"));
     }
 
-    @Test
-    public void remove() {
-    }
+//    @Test
+//    public void remove() {
+//    }
 
     @Test
     public void getFromID() {
+        Meal act = Meal.getFromID(35,c);
+        assertThat(act.getId(),is(35));
+        assertThat(act.getFoodName(),is("apple"));
+        assertThat(act.getQuantity(),is(1));
+        assertThat(act.getCalories(),is(95));
+        assertThat(act.getType(),is("Dinner"));
     }
 
     @Test
     public void checkIfInUse() {
+        Boolean act = a.checkIfInUse(c);
+        assertThat(act,is(true));
+    }
+    @Test
+    public void checkIfInUseNot() {
+        Boolean act = b.checkIfInUse(c);
+        assertThat(act,is(false));
     }
 
-    @Test
-    public void getDays() {
-    }
-
-    @Test
-    public void getDateAll() {
-    }
+//    @Test
+//    public void getDays() {
+//
+//    }
+//
+//    @Test
+//    public void getDateAll() {
+//    }
 }
