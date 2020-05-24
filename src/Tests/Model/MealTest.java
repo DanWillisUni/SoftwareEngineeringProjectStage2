3b6c4b8885dcb2106cf76223d28312a8910b826c
DanWillisUni <dan.willis.uni@gmail.com>
2020-05-24 18:35:57 +0100

    meal database tests

--- src/Tests/Model/MealTest.java
+++ src/Tests/Model/MealTest.java
@@ -1,9 +1,12 @@
 package Model;
 
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -14,22 +17,21 @@ public class MealTest {
     static FoodItem fooda;
     static Meal b;
     static FoodItem foodb;
-    @Before
-    public void setUp() {
+    @BeforeClass
+    public static void setUp() {
         GenericDatabaseController genericController = new GenericDatabaseController();
         c=genericController.getConnection();
         fooda = new FoodItem(0,"apple",95,"");
-        a = new Meal(35,fooda,1,"Dinner");//is in database
+        a = new Meal(300,fooda,1,"Dinner");//is in database
         foodb = new FoodItem(70,"Sandwich",260,"Ham and Cheese");
-        b = new Meal(300,foodb,1,"Lunch");//is not in database
+        b = new Meal(301,foodb,1,"Lunch");//is not in database
     }
 
     @Test
     public void getId() {
         int act = a.getId();
-        assertThat(act,is(35));
+        assertThat(act,is(300));
     }
-
     @Test
     public void getFood() {
         FoodItem act = b.getFood();
@@ -38,35 +40,54 @@ public class MealTest {
         assertThat(act.getAmountOfCalories(),is(foodb.getAmountOfCalories()));
         assertThat(act.getDescription(),is("Ham and Cheese"));
     }
-
     @Test
     public void getQuantity() {
         int act = b.getQuantity();
         assertThat(act,is(1));
     }
-
     @Test
     public void getType() {
         String act = a.getType();
         assertThat(act,is("Dinner"));
     }
-
     @Test
     public void getFoodName() {
         String act = b.getFoodName();
         assertThat(act,is("Sandwich"));
     }
-
     @Test
     public void getCalories() {
         int act = a.getCalories();
         assertThat(act,is(95));
     }
-
-//    @Test
-//    void add() {
-//    }
-
+    @Test
+    public void add() {
+        try(ResultSet rs=c.createStatement().executeQuery("SELECT * FROM softwareengineering.meal where idMeal = 300")) {
+            assertFalse(rs.next());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        a.add(c);
+        try(ResultSet rs=c.createStatement().executeQuery("SELECT * FROM softwareengineering.meal where idMeal = 300")) {
+            assertTrue(rs.next());
+            assertThat(rs.getInt("idMeal"),is(300));
+            assertThat(rs.getInt("idFood"),is(0));
+            assertThat(rs.getInt("quantity"),is(1));
+            assertThat(rs.getString("mealCategory"),is("Dinner"));
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        try {
+            final String query = "DELETE FROM `softwareengineering`.`meal` WHERE (`idmeal` = '300')";
+            try (
+                    PreparedStatement pstmt = c.prepareStatement(query)
+            ){
+                pstmt.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
     @Test
     public void getMeal() {
         Meal act = Meal.getMeal(fooda,1,"Dinner",c);
@@ -76,11 +97,34 @@ public class MealTest {
         assertThat(act.getCalories(),is(95));
         assertThat(act.getType(),is("Dinner"));
     }
-
-//    @Test
-//    public void remove() {
-//    }
-
+    @Test
+    public void remove() {
+        try {
+            final String query = "INSERT into `softwareengineering`.`meal` VALUES (300,0,1,'Dinner')";
+            try (
+                    PreparedStatement pstmt = c.prepareStatement(query)
+            ){
+                pstmt.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try(ResultSet rs=c.createStatement().executeQuery("SELECT * FROM softwareengineering.meal where idMeal = 300")) {
+            assertTrue(rs.next());
+            assertThat(rs.getInt("idMeal"),is(300));
+            assertThat(rs.getInt("idFood"),is(0));
+            assertThat(rs.getInt("quantity"),is(1));
+            assertThat(rs.getString("mealCategory"),is("Dinner"));
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        a.remove(c);
+        try(ResultSet rs=c.createStatement().executeQuery("SELECT * FROM softwareengineering.meal where idMeal = 300")) {
+            assertFalse(rs.next());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
     @Test
     public void getFromID() {
         Meal act = Meal.getFromID(35,c);
@@ -90,24 +134,9 @@ public class MealTest {
         assertThat(act.getCalories(),is(95));
         assertThat(act.getType(),is("Dinner"));
     }
-
-    @Test
-    public void checkIfInUse() {
-        Boolean act = a.checkIfInUse(c);
-        assertThat(act,is(true));
-    }
     @Test
     public void checkIfInUseNot() {
         Boolean act = b.checkIfInUse(c);
         assertThat(act,is(false));
     }
-
-//    @Test
-//    public void getDays() {
-//
-//    }
-//
-//    @Test
-//    public void getDateAll() {
-//    }
 }
